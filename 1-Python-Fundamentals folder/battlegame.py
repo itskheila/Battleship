073719ce020e228c11ec40aca96de14d042b26eb
itskheila <munenetiara@gmail.com>
2025-08-14 21:51:02 +0300

    game movement

--- 1-Python-Fundamentals folder/battlegame.py
+++ 1-Python-Fundamentals folder/battlegame.py
@@ -22,3 +22,28 @@ human_damage = 20
 #Dragon Stats (as integers):
 dragon_hp = 300
 dragon_damage = 50
+
+# Player Choice
+while True:
+    print("1) Wizard")
+    print("2) Elf")
+    print("3) Human")
+    character_choice = input("Choose your character: ")
+    
+    if character_choice == '1':
+        character = wizard
+        my_hp = wizard_hp
+        my_damage = wizard_damage
+        break
+    elif character_choice == '2':
+        character = elf
+        my_hp = elf_hp
+        my_damage = elf_damage
+        break
+    elif character_choice == '3':
+        character = human
+        my_hp = human_hp
+        my_damage = human_damage
+        break
+    else:
+        print("Unknown character")
