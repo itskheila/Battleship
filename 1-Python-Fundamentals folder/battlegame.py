417cd5816e95edc062994cbd70d20219389309a4
itskheila <munenetiara@gmail.com>
2025-08-14 21:38:15 +0300

    battlegame

---
+++ 1-Python-Fundamentals folder/battlegame.py
@@ -0,0 +1,24 @@
+#Characters (as strings):
+wizard = "Wizard"
+elf = "Elf"
+human = "Human"
+
+
+
+#Character HP (as integers):
+wizard_hp = 70
+elf_hp = 100
+human_hp = 150
+
+
+
+#Character Damage (as integers):
+wizard_damage = 150
+elf_damage = 100
+human_damage = 20
+
+
+
+#Dragon Stats (as integers):
+dragon_hp = 300
+dragon_damage = 50
